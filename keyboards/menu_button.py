fb74d9ca3bf46da6939c586b3bb7436d7d2da350
Max Prikul <prikul-v1@yandex.ru>
2023-01-05 21:56:25 +0300

    version 0.1

--- keyboards/menu_button.py
+++ keyboards/menu_button.py
@@ -2,6 +2,7 @@ from aiogram import Dispatcher, types
 
 async def set_main_menu(dp: Dispatcher):
     main_menu_commands = [
+        types.BotCommand(command='/start', description='Начало работы с ботом'),
         types.BotCommand(command='/help', description='Справка по работе бота'),
         types.BotCommand(command='/support', description='Поддержка'),
         types.BotCommand(command='/contacts', description='Другие способы связи')
--- mainBot.py
+++ mainBot.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher, executor, types
@@ -9,47 +10,61 @@ from keyboards.menu_button import set_main_menu
 logger = logging.getLogger(__name__)
 
 
-config = load_config('.env')
 
-bot: Bot = Bot(token=config.tg_bot.token, parse_mode='HTML') # creating object bot
-dp: Dispatcher = Dispatcher(bot) # creating object dp
+async def main():
+    logging.basicConfig(level=logging.INFO, format=u'%(filename)s:%(lineno)d #%(levelname)-8s '
+                        u'[%(asctime)s] - %(name)s - %(message)s')
+    logger.info('Starting bot')
 
-keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(resize_keyboard=True) # creating object keyboard
+    config = load_config('.env')
 
-# creating objects buttons
-button_1: KeyboardButton = KeyboardButton('Dogs')
-button_2: KeyboardButton = KeyboardButton('Lions')
+    bot: Bot = Bot(token=config.tg_bot.token, parse_mode='HTML') # creating object bot
+    dp: Dispatcher = Dispatcher(bot) # creating object dp
 
-keyboard.add(button_1, button_2) #adding buttons
-keyboard.add(KeyboardButton(text='Отправить телефон', request_contact=True))
-keyboard.add(KeyboardButton(text='Отправить геолокацию', request_location=True))
-keyboard.add(KeyboardButton(text='Start Web App', web_app=WebAppInfo(url="https://stepik.org/")))
+    keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(resize_keyboard=True) # creating object keyboard
 
-async def process_start_command(message: types.Message):
-    await message.answer('What scares u more ?', reply_markup=keyboard) # /start
+    # creating objects buttons
+    button_1: KeyboardButton = KeyboardButton('Dogs')
+    button_2: KeyboardButton = KeyboardButton('Lions')
 
-async def process_dog_answer(message: types.Message):
-    await message.answer('Dogs are the best creatures', reply_markup=ReplyKeyboardRemove()) # dogs answer + chat bar closing
+    keyboard.add(button_1, button_2) #adding buttons
+    keyboard.add(KeyboardButton(text='Отправить телефон', request_contact=True))
+    keyboard.add(KeyboardButton(text='Отправить геолокацию', request_location=True))
+    keyboard.add(KeyboardButton(text='Start Web App', web_app=WebAppInfo(url="https://stepik.org/")))
 
-async def process_lion_answer(message: types.Message):
-    await message.answer('Lions pretty scary =(', reply_markup=ReplyKeyboardRemove()) # lions answer + chat bar closing
+    async def process_start_command(message: types.Message):
+        await message.answer('What scares u more ?', reply_markup=keyboard) # /start
 
-dp.register_message_handler(process_start_command, commands='start')
-dp.register_message_handler(process_dog_answer, text='Dogs')
-dp.register_message_handler(process_lion_answer, text='Lions')
+    async def process_dog_answer(message: types.Message):
+        await message.answer('Dogs are the best creatures', reply_markup=ReplyKeyboardRemove()) # dogs answer + chat bar closing
 
-# @dp.message_handler(commands=['start'])
-# async def process_start_command(message: types.Message):
-#     await message.answer('zxc\nsad')
-#
-# @dp.message_handler(commands=['help'])
-# async def process_help_commend(message: types.Message):
-#     await message.answer('kill me')
-#
-# @dp.message_handler()
-# async def send_echo(message: types.Message):
-#     await message.reply(message.text)
+    async def process_lion_answer(message: types.Message):
+        await message.answer('Lions pretty scary =(', reply_markup=ReplyKeyboardRemove()) # lions answer + chat bar closing
+
+    dp.register_message_handler(process_start_command, commands='start')
+    dp.register_message_handler(process_dog_answer, text='Dogs')
+    dp.register_message_handler(process_lion_answer, text='Lions')
+
+    # @dp.message_handler(commands=['start'])
+    # async def process_start_command(message: types.Message):
+    #     await message.answer('zxc\nsad')
+    #
+    # @dp.message_handler(commands=['help'])
+    # async def process_help_commend(message: types.Message):
+    #     await message.answer('kill me')
+    #
+    # @dp.message_handler()
+    # async def send_echo(message: types.Message):
+    #     await message.reply(message.text)
+    await set_main_menu(dp)
+
+    try:
+        await dp.start_polling()
+    finally:
+        await bot.close()
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
-
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.error('Bot was stopped')
