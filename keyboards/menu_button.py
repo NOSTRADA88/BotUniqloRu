92f0260748d26ecb1e38c2fb5ca2648c9433e16a
Max Prikul <prikul-v1@yandex.ru>
2023-01-26 22:41:47 +0300

    start

--- keyboards/menu_button.py
+++ keyboards/menu_button.py
@@ -1,4 +1,6 @@
 from aiogram import Dispatcher, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+
 
 async def set_main_menu(dp: Dispatcher):
     main_menu_commands = [
@@ -8,3 +10,20 @@ async def set_main_menu(dp: Dispatcher):
         # types.BotCommand(command='/contacts', description='Другие способы связи')
     ]
     await dp.bot.set_my_commands(main_menu_commands)
+
+
+test_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+test_button1 = KeyboardButton(text="Тестовая кнопка номер 1")
+test_button2 = KeyboardButton(text="Тестовая кнопка номер 2")
+test_button3 = KeyboardButton(text="Тестовая кнопка номер 3")
+test_button4 = KeyboardButton(text="Тестовая кнопка номер 4")
+test_kb.add(test_button1).add(test_button2).add(test_button3, test_button4)
+
+
+test_ikb = InlineKeyboardMarkup(row_width=2)
+inline_button1 = InlineKeyboardButton(text="Инлайн кнопка 1", callback_data="button1")
+inline_button2 = InlineKeyboardButton(text="Инлайн кнопка 2", callback_data="button2")
+inline_button3 = InlineKeyboardButton(text="Инлайн кнопка 3", url="https://github.com/aiogram/deta/issues")
+inline_button4 = InlineKeyboardButton(text="Инлайн кнопка 4", url="https://github.com/settings/profile")
+test_ikb.add(inline_button1).add(inline_button2).add(inline_button3, inline_button4)
+
--- mainBot.py
+++ mainBot.py
@@ -1,12 +1,12 @@
 from asyncio import new_event_loop
-
+from aiogram.dispatcher.filters import Text
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
 from BotUniqloRu.config import load_config
 from keyboards.menu_button import set_main_menu
 from motor.motor_asyncio import AsyncIOMotorClient
 from datetime import datetime
-
+from keyboards.menu_button import test_ikb, test_kb
 config = load_config('.env')
 
 loop = new_event_loop()
@@ -27,16 +27,16 @@ keyboard.add(button_1, button_2) #adding buttons
 client = AsyncIOMotorClient(config.db_link.link)
 collection = client.UniqloThailand.clothes
 
-async def add_user(user_id):
-    date = datetime.now().date()
-    collection.insert_one({
-        "_id": user_id,
-        "date": str(date)
-    })
+# async def add_user(user_id):
+#     date = datetime.now().date()
+#     collection.insert_one({
+#         "_id": user_id,
+#         "date": str(date)
+#     })
 async def process_start_command(message: types.Message):
-    await message.answer('how scared u most?', reply_markup=keyboard) # /start
+    await message.answer('how scared u most?', reply_markup=test_kb) # /start
     user_id = message.chat.id
-    await add_user(user_id)
+    # await add_user(user_id)
 async def process_dog_answer(message: types.Message):
     await message.answer('Dogs are the best creatures', reply_markup=ReplyKeyboardRemove()) # dogs answer + chat bar closing
 
@@ -46,6 +46,35 @@ async def process_lion_answer(message: types.Message):
 dp.register_message_handler(process_start_command, commands='start')
 dp.register_message_handler(process_dog_answer, text='Dogs')
 dp.register_message_handler(process_lion_answer, text='Lions')
+
+
+@dp.message_handler(Text(equals="Тестовая кнопка номер 1"))
+async def kb_button1(messasge: types.Message):
+    await messasge.answer(text="Ты нажал на кнопку 1", reply_markup=test_ikb)
+
+
+@dp.message_handler(Text(equals="Тестовая кнопка номер 2"))
+async def kb_button1(messasge: types.Message):
+    await messasge.answer(text="Ты нажал на кнопку 2", reply_markup=test_ikb)
+
+
+@dp.message_handler(Text(equals="Тестовая кнопка номер 3"))
+async def kb_button1(messasge: types.Message):
+    await messasge.answer(text="Ты нажал на кнопку 3", reply_markup=test_ikb)
+
+
+@dp.message_handler(Text(equals="Тестовая кнопка номер 4"))
+async def kb_button1(messasge: types.Message):
+    await messasge.answer(text="Ты нажал на кнопку 4", reply_markup=test_ikb)
+
+
+@dp.callback_query_handler()
+async def inline_test(callback: types.CallbackQuery):
+    if callback.data == "button1":
+        await callback.message.answer(text="Ты нажал на инлайн кнопку 1")
+    if callback.data == "button2":
+        await callback.message.answer(text="Ты нажал на инлайн кнопку 2")
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
